e4413873fde0fbc4ed06a5371bd2d3736b188c2c
Butterfly <daniela.kucharczyk@gmx.de>
2013-05-14 18:54:42 +0200

    Name geändert

---
+++ wba2-2/src/webservice/DozentRestWebservice.java
@@ -0,0 +1,15 @@
+package webservice;
+
+import javax.ws.rs.Path;
+
+import Dozenten.Dozenten;
+
+@Path("/classDozent")
+public class DozentRestWebservice {
+
+	/**Dozent laden
+	 * 
+	 * @param id Eine ID des Dozenten der zurückgegeben werden soll
+	 * @return Dozent
+	 */
+}
