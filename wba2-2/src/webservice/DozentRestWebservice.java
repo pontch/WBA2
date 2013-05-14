087aa2b0ad585e633471b7c193918136b7dfe0a9
Butterfly <daniela.kucharczyk@gmx.de>
2013-05-14 19:17:25 +0200

    Änderung

--- wba2-2/src/webservice/DozentRestWebservice.java
+++ wba2-2/src/webservice/DozentRestWebservice.java
@@ -1,15 +1,1 @@
-package webservice;
 
-import javax.ws.rs.Path;
-
-import Dozenten.Dozenten;
-
-@Path("/classDozent")
-public class DozentRestWebservice {
-
-	/**Dozent laden
-	 * 
-	 * @param id Eine ID des Dozenten der zurückgegeben werden soll
-	 * @return Dozent
-	 */
-}
